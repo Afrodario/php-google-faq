e2bf1927fabb2fb7e51a0564178cc592e5d1d6d1
Dario La Tegola <dariolategola@gmail.com>
2022-03-11 15:02:33 +0100

    Minima stilizzazione grafica

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercitazione PHP Google Faq</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <header>
+        <h1>Privacy e Termini</h1>
+        <ul>
+            <li>Introduzione</li>
+            <li>Norme sulla privacy</li>
+            <li>Termini di servizio</li>
+            <li>Tecnologie</li>
+            <li>Domande frequenti</li>
+        </ul>
+    </header>
+
+    <main>
+        <div class="container">
+            
+        </div>
+    </main>
+</body>
+</html>
